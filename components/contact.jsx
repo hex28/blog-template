558503944ea039512b0241008ce54b959226a500
andy <huynha859@gmail.com>
2018-02-21 14:51:40 -0800

    fixed contact forms

--- components/contact.jsx
+++ components/contact.jsx
@@ -4,22 +4,22 @@ export default class Contact extends React.Component {
 
   render() {
     return (
-    <div class="jumbotron d-flex align-items-center">
+    <div className="jumbotron d-flex align-items-center">
       <div className="container text-center">
         <h1><i className="icon-envelope sm-icons"></i></h1>
 
         <form>
-          <div class="form-group">
-            <input type="text" class="form" id="exampleInputPassword1" placeholder="Name"/>
+          <div className="form-group">
+            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Name"/>
           </div><br/>
-          <div class="form-group">
-            <input type="email" class="form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          <div className="form-group">
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div><br/>
-          <div class="form-group">
-            <textarea type="password" class="form" id="exampleInputPassword1" placeholder="message"></textarea>
+          <div className="form-group">
+            <textarea type="password" className="form-control" id="exampleInputPassword1" placeholder="message"></textarea>
           </div>
-          <button type="submit" class="btn btn-dark btn-lg">Submit</button>
+          <button type="submit" className="btn btn-dark btn-lg">Submit</button>
         </form>
       </div>
       </div>
